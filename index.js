8092a79dd505296e4c3e451c3169479963770022
Tim De Pauw <tim@pwnt.be>
2014-06-05 00:40:38 +0200

    Slight logic cleanup; bump

--- index.js
+++ index.js
@@ -12,33 +12,36 @@ var wordRE = XRegExp('\\b((\\p{L})(\\p{L}*))', 'g');
 
 var replacer = function(str) {
   var fn = function(match, word, first, rest, offset) {
-    // If the match is preceded by a letter and an apostrophe, we are dealing
-    // with a genitive. Do not capitalize.
+    var shouldCapitalize;
     if (offset > 1 &&
         isApostrophe(fn.string[offset - 1]) &&
         isLetter(fn.string[offset - 2])) {
-      return word;
-    }
-    var shouldCapitalize;
-    // The first match is always capitalized.
-    if (fn.firstOffset < 0) {
+      // If the match is preceded by a letter and an apostrophe, we are dealing
+      // with a genitive. Do not capitalize.
+      shouldCapitalize = false;
+    } else if (fn.firstOffset < 0) {
+      // The first match is always capitalized.
       shouldCapitalize = true;
+      // Hold on to the match's position.
       fn.firstOffset = offset;
     } else {
       // Only capitalize if the word isn't a minor.
       shouldCapitalize = !minors.hasOwnProperty(word.toLowerCase());
     }
-    if (shouldCapitalize) {
-      // We already capitalize, so we don't need to hold on to the index.
-      fn.lastOffset = -1;
-      // Perform capitalization.
-      return first.toUpperCase() + rest;
-    } else {
-      // Keep the index of the last word, in case we still need to capitalize.
+
+    if (!shouldCapitalize) {
+      // Keep the position of the last word we encountered, because we need to
+      // capitalize it when we are done.
       fn.lastOffset = offset;
       // Don't touch the word for now.
       return word;
     }
+
+    // Even if this is the last word, we are already about to capitalize it, so
+    // we don't need to hold on to its position.
+    fn.lastOffset = -1;
+    // Perform capitalization.
+    return first.toUpperCase() + rest;
   };
   fn.string = str;
   fn.firstOffset = -1;
